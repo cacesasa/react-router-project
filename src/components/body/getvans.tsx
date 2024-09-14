d26d9dbfc114c33620f8c3a93649a0c27c05f0d5
Carlos Santana <cacesasa@gmail.com>
2024-09-14 17:59:50 +0200

    Added the final van page

--- src/components/body/getvans.tsx
+++ src/components/body/getvans.tsx
@@ -17,17 +17,12 @@ export default function GetVans({data}) {
         <div className={styles.van}>
           <Link to={`/vans/${van.id}`}>
             <img className={styles.vanImg} src={van.imageUrl} alt={van.name} />
-          </Link>
+          </Link>         
+            
           <div className={styles.vanInfo}>
-            <Link to={`/vans/${van.id}`}>
-              <h2 className={styles.vanName}>{van.name}</h2>
-            </Link>
-            <Link to={`/vans/${van.id}`}>
-              <h3 className={styles.vanPrice}>${van.price}<p className={styles.perDay}>/day</p>
-            </h3></Link>
-            <Link to={`/vans/${van.id}`}>
-              <p className={styles[van.type]}>{van.type}</p>
-            </Link>
+            <Link to={`/vans/${van.id}`}><h2 className={styles.vanName}>{van.name}</h2></Link>        
+            <Link to={`/vans/${van.id}`}><h3 className={styles.vanPrice}>${van.price}<p className={styles.perDay}>/day</p></h3></Link>             
+            <Link to={`/vans/${van.id}`}><p className={styles[van.type]}>{van.type}</p></Link>
           </div>
         </div>
       ))}
--- src/components/body/vanPage.tsx
+++ src/components/body/vanPage.tsx
@@ -1,16 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import styles from './vanpage.module.css'
 
 export default function VanPage({data}) {
+    const params = useParams();
+    const details = data.find((van) => van.id === params.id);
     return (
         <div className={styles.vanPage}>
-            <Link to={'/vans/'}>Back to all vans</Link>
-            <img className={styles.vanImg} src={data[0].imageUrl} alt={data[0].name} />
-            <p className={styles.type}>{data[0].type}</p>
-            <h1 className={styles.vanName}>{data[0].name}</h1>
-            <p className={styles.vanPrice}>${data[0].price}<span>/day</span></p>
-            <p className={styles.description}>{data[0].description}</p>
-            <Link className={styles.link} to={`/vans/${data[0].id}/book`}>Book now</Link>              
+            <Link to={'/vans/'} className={styles.link}>Back to all vans</Link>
+            <img className={styles.vanImg} src={details.imageUrl} alt={details.name} />
+            <div className={styles.vanInfo}>
+                <p className={styles[details.type]}>{details.type}</p>
+                <h1 className={styles.vanName}>{details.name}</h1>
+                <p className={styles.vanPrice}>${details.price}<span>/day</span></p>
+                <p className={styles.description}>{details.description}</p>
+            </div>
+            <Link className={styles.link} to={`/vans/${details.id}/book`}><button className={styles.button}>Rent this van</button></Link>              
 
         </div>
     )
